65ced78f61ab380b192b9e8ef1f32fd721ad8cff
Susumu OTA <1632335+susumuota@users.noreply.github.com>
2023-02-09 02:55:03 +0900

    fix: loop forever.

--- keygen.ts
+++ keygen.ts
@@ -1,4 +1,4 @@
-import nostr from 'npm:nostr-tools@1.2.1';
+import nostr from 'npm:nostr-tools@1.2.3';
 
 const generateKey = (prefix: string) => {
   const prefixLen = prefix.length;
@@ -8,7 +8,7 @@ const generateKey = (prefix: string) => {
     const s = npub.substr(5, prefixLen);
     if (s === prefix) {
       const nsec = nostr.nip19.nsecEncode(sk);
-      return { nsec, npub };
+      console.log({ nsec, npub });
     }
   }
 };
@@ -17,7 +17,7 @@ const generateKey = (prefix: string) => {
 const checkPrefix = (prefix: string) => /^[02-9ac-hj-np-z]+$/.test(prefix);
 
 if (Deno.args.length === 0 || Deno.args.length === 1 && checkPrefix(Deno.args[0])) {
-  console.log(generateKey(Deno.args[0] ?? ''));
+  generateKey(Deno.args[0] ?? '');
 } else {
   console.log(
     'Usage: deno run keygen.ts [PREFIX]\n\n  PREFIX must be lower-case alphanumeric except 1, b, i, o.',
